50bd70bf48f3b99e4e8beff4cd9e995419b4d7e4
Newproject011 <[200~keerthimasmile@gmail.com~>
2022-12-12 14:24:52 +0530

    given message

---
+++ src/test/java/org/tcs/test/login/TrialClass.java
@@ -0,0 +1,20 @@
+package org.tcs.test.login;
+
+public class TrialClass {
+	private void method1() {
+		System.out.println("method1");
+	}
+	private void method2() {
+		System.out.println("method2");
+
+	}
+	private void method3() {
+		System.out.println("method3");
+
+	}
+	private void method4() {
+		System.out.println("method4");
+
+	}
+
+}
